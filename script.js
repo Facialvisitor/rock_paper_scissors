92f9a85f52d2bc29ca2de7b3070ad2f859e88cdc
Rico <168263779+Facialvisitor@users.noreply.github.com.>
2026-02-21 10:49:57 +0100

    add the logic for the player scores and declare the players score variables

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ function getComputerChoice(){
    else return "scissors"
 }
 console.log(getComputerChoice())
+
+// Create a new function named getHumanChoice.
+// Write the code so that getHumanChoice will,
+// return one of the valid choices depending on what the user inputs
+// assuming the user always enters a valid choice(no reprompting)
+function getHumanChoice(){
+    let choose = prompt("Enter your choice: rock, paper, or scissors")
+    return choose.toLowerCase().trim()
+}
+//.toLowerCase for capitalisation and case sensitivity,
+// trim(), to accomodate empty space promted by the user
+console.log(getHumanChoice())
+
+// Create two new variables named humanScore and computerScore in the global scope.
+// Initialize those variables with the value of 0.
+
+let humanScore = 0;
+let computerScore = 0;
